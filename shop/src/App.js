d86a6f8ddd25d4d67a6c1be67f72460a59b48c0f
soulhn <vldnrlf123@naver.com>
2022-09-23 17:40:20 +0900

    노트북에서 집으로

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">설핑몰</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">홈으로</Nav.Link>
+            <Nav.Link href="#features">상품</Nav.Link>
+            <Nav.Link href="#pricing">질문 </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Product shoes={shoes[i]} i={i}></Product>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" alt="Shoes 1" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
