c511df0a908ddfc3cc67c50601f6744547cc30cf
soulhn <vldnrlf123@naver.com>
2022-12-04 23:58:06 +0900

    codingapple day1

--- codingAppleReact/blog/src/App.js
+++ codingAppleReact/blog/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그 프로젝트 시작</h4>
+        <h4 style={{ color: "red", fontSize: "16px" }}>블로그 프로젝트 시작</h4>
       </div>
       <h4>블로그 글 제목 = {post}</h4>
     </div>
