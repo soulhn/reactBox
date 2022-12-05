890c4091e47d2e4cb1f07a76172ff0f950191320
soulhn <vldnrlf123@naver.com>
2022-12-05 23:54:44 +0900

    coding apple day2

--- codingAppleReact/blog/src/App.js
+++ codingAppleReact/blog/src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [a, b] = useState("남자 코트 추천");
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{ color: "red", fontSize: "16px" }}>블로그 프로젝트 시작</h4>
       </div>
-      <h4>블로그 글 제목 = {post}</h4>
+      <div className="list">
+        <h4>블로그 글 제목 = {post}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>블로그 글 제목 = {a}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>블로그 글 제목은 {post}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
