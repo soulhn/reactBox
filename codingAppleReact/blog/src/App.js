6ef38347637f8ec44ee55721f60abb2c83f5e153
soulhn <vldnrlf123@naver.com>
2022-12-06 22:02:46 +0900

    coding appy day3

--- codingAppleReact/blog/src/App.js
+++ codingAppleReact/blog/src/App.js
@@ -1,27 +1,54 @@
+// eslint-disable
+
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let post = "강남 우동 맛집";
-  let [a, b] = useState("남자 코트 추천");
+  let [postTitle, postTitleChanger] = useState(["가가가", "나나나", "다다다"]); //글 제목
+  let [like, likeChanger] = useState(0); // 좋아요 저장 공간
+
+  function likeCounter() {
+    likeChanger(like + 1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{ color: "red", fontSize: "16px" }}>블로그 프로젝트 시작</h4>
       </div>
+      {/* 프로젝트 헤더 부분 끝 */}
+      <button
+        onClick={() => {
+          let copy = [...postTitle];
+          copy[0] = "여자 코트 추천";
+          postTitleChanger(copy);
+        }}
+      >
+        글 수정
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...postTitle];
+          copy.sort();
+          postTitleChanger(copy);
+        }}
+      >
+        가나다순정렬
+      </button>
       <div className="list">
-        <h4>블로그 글 제목 = {post}</h4>
+        <h4>
+          {postTitle[0]} <span onClick={likeCounter}>❤</span> {like}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className="list">
-        <h4>블로그 글 제목 = {a}</h4>
+        <h4>{postTitle[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className="list">
-        <h4>블로그 글 제목은 {post}</h4>
+        <h4>{postTitle[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
