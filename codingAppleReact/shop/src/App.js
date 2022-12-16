e3f56e3cabc058e04c2f294bb1c02b33fd097ab2
soulhn <vldnrlf123@naver.com>
2022-12-16 23:37:52 +0900

    coding apple day11

---
+++ codingAppleReact/shop/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>{" "}
+    </div>
+  );
+}
+
+export default App;
